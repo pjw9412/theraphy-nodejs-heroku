6caf71ae78534dbec3d591e8bd0a2291690fc7d0
pjw9412 <pjwts9412@gmail.com>
2020-10-24 08:13:02 +0900

    change server

--- module/axiosRequest.js
+++ module/axiosRequest.js
@@ -10,8 +10,8 @@ const axiosRequest = async filePath => {
         // flask API와의 통신인듯.
         var response = await axios
             .create({ headers: formData.getHeaders() })
-            .post('http://localhost:5000/predict', formData);      // Local
-            // .post('https://theraphy-flask.herokuapp.com/predict', formData);   // Server
+            // .post('http://localhost:5000/predict', formData);      // Local
+            .post('https://theraphy-nodejs-heroku2.herokuapp.com/predict', formData);   // Server
 
         // predict는 flask폴더 내 rest.py의 predict()와 연관이 있는 듯.
         // .post('http://joyuriz-api:5000/predict', formData);
