dc188e0e3498ef142b3a4bddf11b07152f93daf1
pjw9412 <pjwts9412@gmail.com>
2020-10-24 07:40:14 +0900

    start

---
+++ module/axiosRequest.js
@@ -0,0 +1,24 @@
+const FormData = require('form-data');
+const axios = require('axios');
+const fs = require('fs');
+const axiosRequest = async filePath => {
+    var newFile = fs.createReadStream(filePath);
+    const formData = new FormData();
+    formData.append('image', newFile, newFile.name);
+    try {
+        console.log('TRY');
+        // flask API와의 통신인듯.
+        var response = await axios
+            .create({ headers: formData.getHeaders() })
+            .post('http://localhost:5000/predict', formData);      // Local
+            // .post('https://theraphy-flask.herokuapp.com/predict', formData);   // Server
+
+        // predict는 flask폴더 내 rest.py의 predict()와 연관이 있는 듯.
+        // .post('http://joyuriz-api:5000/predict', formData);
+        return response.data;
+    } catch (e) {
+        console.log('[ERROR|axiosRequest] ', e);
+        return e;
+    }
+};
+module.exports = axiosRequest;
---
+++ server.js
@@ -0,0 +1,118 @@
+var express = require('express');
+var app = express();
+// const fs = require('fs');
+const md5File = require('md5-file');
+const axiosRequest = require('./module/axiosRequest.js');
+const cors = require('cors');
+const multerOption = require('./module/multerOption.js'); // 파일 업로드 관련 모듈
+const port = process.env.PORT || 3001;
+
+app.use(cors()); // use cors middleware
+
+// post(url, data, config?)
+app.post('/', multerOption.single('image'), async (requset, response) => {
+    const fileName = requset.file['filename'];
+    const hash = md5File.sync('userUpload/' + fileName);
+    console.log('FILE NAME = ', fileName);
+
+    try {
+        // const imageInfo = [
+        //     {
+        //         success: true,
+        //         hash: hash,
+        //         predictions: 'House',
+        //         coordinate:{
+        //             x: 100,
+        //             y: 100,
+        //             width: 300,
+        //             height: 300,
+        //         },
+        //         path: fileName,
+        //         voteChaewon: 0,
+        //         voteYuri: 0,
+        //         voteYena: 0,
+        //         request: 1,
+        //         house: [4, 5, 7, 8, 9, 12, 18, 19, 22, 23, 24, 27, 31]
+        //     },
+        //     {
+        //         hash: hash,
+        //         predictions: 'Window',
+        //         coordinate:{
+        //             x: 200,
+        //             y: 200,
+        //             width: 300,
+        //             height: 300,
+        //         }
+        //     },
+        //     {
+        //         hash: hash,
+        //         predictions: 'Door',
+        //         coordinate:{
+        //             x: 300,
+        //             y: 300,
+        //             width: 300,
+        //             height: 300,
+        //         },
+        //     }
+        // ];
+        // response.send(imageInfo);
+
+        // axiosRequest => flask API와의 통신
+        const axiosResponse = await axiosRequest('userUpload/' + fileName);
+        console.log("AFTER axiosRequest")
+        if (axiosResponse['success'] === true) {
+            console.log("axiosRequest SUCCESS");
+            let imageInfo=[];
+            for(let i = 0; i < axiosResponse.label.length; i++){
+                if(i == 0){ // 첫 번째 배열에는 모든 항목을 set
+                    imageInfo[i] = [
+                        {
+                            success: true,
+                            hash: hash,
+                            coordinate:{
+                                // LTRB = x1,y1 x2,y2
+                                x: axiosResponse['x1'][i],
+                                y:  axiosResponse['y1'][i],
+                                width: axiosResponse['x2'][i]-axiosResponse['x1'][i],
+                                height: axiosResponse['y2'][i]-axiosResponse['y1'][i],
+                            },
+                            label : axiosResponse['label'][i],
+                            score : axiosResponse['score'][i],
+                            path: fileName,
+                            voteChaewon: 0,
+                            voteYuri: 0,
+                            voteYena: 0,
+                            request: 1,
+                            house: [4, 5, 7, 8, 9, 12, 18, 19, 22, 23, 24, 27, 31]
+                        }
+                    ];
+                }else{  // 이후 배열에는 일부만 set
+                    imageInfo[i] = [
+                        {
+                            coordinate:{
+                                x: axiosResponse['x1'][i],
+                                y:  axiosResponse['y1'][i],
+                                width: axiosResponse['x2'][i]-axiosResponse['x1'][i],
+                                height: axiosResponse['y2'][i]-axiosResponse['y1'][i],
+                            },
+                            label : axiosResponse['label'][i],
+                            score : axiosResponse['score'][i]
+                        }
+                    ];
+                }
+            }
+            response.json(imageInfo);
+        } else {
+            response.json({
+                success: false,
+                path: fileName,
+                hash: hash
+            });
+        }
+    } catch (e) {
+        console.log('[ERROR|axiosResponse] : ', e);
+    }
+});
+
+// 3001번 포트넘버를 가진 서버 생성
+app.listen(port, () => console.log(`listening on port ${port}!`));
