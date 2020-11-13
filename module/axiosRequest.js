e3a615171ac5cb38cd0857c6257fb86ef29c8dc9
pjw9412 <pjwts9412@gmail.com>
2020-11-13 11:32:05 +0900

    EC2 link update

--- module/axiosRequest.js
+++ module/axiosRequest.js
@@ -12,7 +12,8 @@ const axiosRequest = async filePath => {
         var response = await axios
             .create({ headers: formData.getHeaders() })
             //.post('http://localhost:5000/predict', formData);      // Local
-            .post('http://54.180.114.163:5000/predict',formData);          // AWS EC2
+            //.post('http://54.180.114.163:5000/predict',formData);          // old AWS EC2
+            .post('htpps://13.209.65.139:5000/predict',formData);
             // .post('https://theraphy-flask-heroku2.herokuapp.com/predict', formData);   // Server
             //.post('https://e3kss2gr7l.execute-api.ap-northeast-2.amazonaws.com/dev/lambda_efs', formData); // REST-API GATEWAY-LAMBDA
             //.post('https://3pvwvh59mk.execute-api.ap-northeast-2.amazonaws.com/lambda_efs',formData); // HTP-API GATEWAY-LAMBDA
